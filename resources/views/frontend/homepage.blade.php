ae238800b1c5b43a22c389e360bd82131c474756
unknown <johirul35-257@diu.edu.bd>
2023-03-29 23:39:42 +0600

    Hero Section Design Update

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -1,20 +1,35 @@
 @extends('frontend.layouts.app')
 
 @section('styles')
-<style>
+    <style>
 
-</style>
+    </style>
 @endsection
 @section('content')
-      <!-- Hero section -->
+    <!-- Hero section -->
     <section class="hero">
         <div class="container">
             <div class="get-started">
                 <p>Get Started from today</p>
             </div>
-            <h1>Join our team today to take your <br>
-                <span class="text-primary">next step</span> of your career.
-            </h1>
-        </div>
+            <div>
+                <h1>Join our team today to take your <br>
+                    <span class="text-primary">next step</span> of your career.
+                </h1>
+            </div>
+            <div class="hero-message">
+                <p>
+                    It is a long established fact that a reader will be distracted by the readable content of a page
+                    when looking at its layout.dsadasd sadsaasdasdasdas dsadas das
+            </div>
+            <div class="Search-box">
+                <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" />
+                    <label class="form-label" for="form1">Search</label>
+                </div>
+                <button type="button" class="btn btn-primary">
+                    <i data-feather="search"></i>
+                </button>
+            </div>
     </section>
 @endsection
