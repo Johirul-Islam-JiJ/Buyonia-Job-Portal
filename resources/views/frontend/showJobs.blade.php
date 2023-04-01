d5184237b37c5c8d5840c0a45edb585b7bad0197
unknown <johirul35-257@diu.edu.bd>
2023-04-01 07:36:15 +0600

    Job Show Page Design Upddate

--- resources/views/frontend/showJobs.blade.php
+++ resources/views/frontend/showJobs.blade.php
@@ -1,3 +1,39 @@
-<h1>
-    show jobs
-</h1>
+@extends('frontend.layouts.app')
+
+@section('styles')
+    <style>
+
+    </style>
+@endsection
+@section('content')
+    <section class="align-center">
+        <div class="container">
+            <div class="job-show-header">
+                <h1>{{ $job->title }}</h1>
+                <div class="d-flex justify-content-center">
+                    <span class="card-text" style="margin-right:40px;">
+                        Job Type: {{ $job->type }}
+                    </span>
+                    <span class="card-text">
+                        No of Vacancies:{{ $job->vacancy }}
+                    </span>
+                </div>
+            </div>
+            <div class="show-job-img">
+                <img src="" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="job-details-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h3>hello</h3>
+                </div>
+                <div class="col-md-4">
+                    <h3>hi</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
