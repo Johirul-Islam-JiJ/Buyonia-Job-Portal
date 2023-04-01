11dc1e02665af8db905065c792460a8654e40429
unknown <johirul35-257@diu.edu.bd>
2023-04-01 06:11:48 +0600

    Card Design Complete

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
+
 class FrontendController extends Controller
 {
     public function index()
     {
-        return view('frontend.homepage');
+        $jobs = Job::orderBy('id', 'desc')->take(4)->get();
+        return view('frontend.homepage',compact('jobs'));
     }
 }
--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -39,104 +39,104 @@
 
     </section>
     <section class="align-center">
-        <div>
-            <h1>Explore Popular
-                <span class="text-primary">Jobs</span>
-            </h1>
-        </div>
-        <div class="hero-message">
-            <p>
-                Get the fastest application so that your name is above other application
-        </div>
-        <div>
-            <ul class="category">
-                <li class="category-link-active">
-                    <a class="category-link" aria-current="page" href="#">Recent Jobs</a>
-                </li>
-                <li class="category-item">
-                    <a class="category-link" href="#">Web Developer</a>
-                </li>
-                <li class="category-item">
-                    <a class="category-link" href="#">Graphic Designer</a>
-                </li>
-                <li class="category-item">
-                    <a class="category-link" href="#">Digital Marketer</a>
-                </li>
-                <li class="category-item">
-                    <a class="category-link" href="#">Mobile App Developer</a>
-                </li>
-            </ul>
-        </div>
-        <div class="card-grid-list">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">UI/UX Designer</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Mobile App Dev</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+        <div class="container">
+            <div>
+                <h1>Explore Popular
+                    <span class="text-primary">Jobs</span>
+                </h1>
             </div>
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">UI/UX Designer</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Mobile App Dev</h5>
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
+            <div class="hero-message">
+                <p>
+                    Get the fastest application so that your name is above other application
             </div>
-        </div>
-        <div class="load-more">
-            <a class="" href="#">Load More</a>
-        </div>
-        <div class="register-msg-img">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="register-msg">
-                        <h1>
-                            So Many People Are <br>
-                            <span class="text-primary">Engaged</span> All Over The World
-                        </h1>
-                        <p> Et in sapien enim congue interdum pulvinar non sed. Ac augue netus tellus semper.Et in sapien
-                            enim congue interdum pulvinar non sed.
-                            Ac augue netus tellus semper. </p>
-                        <img src="frontend/images/Vector.png" alt="">
-
-                    </div>
-                    <div class="register-today">
-                        <a class="" href="#">Register Today</a>
-                    </div>
+            <div>
+                <ul class="category">
+                    <li class="category-link-active">
+                        <a class="category-link" aria-current="page" href="#">Recent Jobs</a>
+                    </li>
+                    <li class="category-item">
+                        <a class="category-link" href="#">Web Developer</a>
+                    </li>
+                    <li class="category-item">
+                        <a class="category-link" href="#">Graphic Designer</a>
+                    </li>
+                    <li class="category-item">
+                        <a class="category-link" href="#">Digital Marketer</a>
+                    </li>
+                    <li class="category-item">
+                        <a class="category-link" href="#">Mobile App Developer</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-grid-list">
+                <div class="row">
+                    @foreach ($jobs as $job)
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between">
+                                        <h5 class="card-title">{{ $job->title }}</h5>
+                                        <p>Posted 11 Hours Ago</p>
+                                    </div>
+                                    <div class="d-flex justify-content-start">
+                                        <span class="card-text" style="margin-right:40px;">
+                                            <i class="fa-brands fa-staylinked"></i>
+                                            {{ $job->company_name }}
+                                        </span>
+                                        <span class="card-text">
+                                            <i class="fa-solid fa-location-dot"></i>
+                                            {{ $job->location }}
+                                        </span>
+                                    </div>
+                                    <div>
+                                        <span>
+                                            <i class="fa-solid fa-money-check-dollar" style="margin-right:10px;"></i>
+                                            <i class="fa-solid fa-bangladeshi-taka-sign"></i>
+                                            {{ $job->salary }}
+                                        </span>
+                                    </div>
+                                    <div class="d-flex justify-content-start">
+                                        <span class="job-category">{{ $job->type }}</span>
+                                        <span class="job-nature">{{ $job->job_nature }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
 
                 </div>
-                <div class="col-sm-6">
-                    <div class="register-img">
-                        <img src="frontend/images/register.png" alt="image">
+            </div>
+            <div class="load-more">
+                <a class="" href="#">Load More</a>
+            </div>
+            <div class="register-msg-img">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="register-msg">
+                            <h1>
+                                So Many People Are <br>
+                                <span class="text-primary">Engaged</span> All Over The World
+                            </h1>
+                            <p> Et in sapien enim congue interdum pulvinar non sed. Ac augue netus tellus semper.Et in
+                                sapien
+                                enim congue interdum pulvinar non sed.
+                                Ac augue netus tellus semper. </p>
+                            <img src="frontend/images/Vector.png" alt="">
+                            <div class="register-today">
+                                <a class="" href="#">Register Today</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="register-img">
+                            <img src="frontend/images/register.png" alt="image">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </section>
 @endsection
