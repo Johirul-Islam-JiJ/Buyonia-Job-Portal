22a3237b5309011b4dc661d5f371f5a66978ede8
unknown <johirul35-257@diu.edu.bd>
2023-04-11 04:52:45 +0600

    Show Job Image Responsive

--- resources/views/frontend/showJobs.blade.php
+++ resources/views/frontend/showJobs.blade.php
@@ -9,21 +9,31 @@
     <section class="align-center">
         <div class="container">
             <div class="job-show-header">
-                <h1>{{ $job->title }}</h1>
                 <div class="d-flex justify-content-center">
-                    <span class="card-text" style="margin-right:40px;">
-                        Job Type: {{ $job->type }}
-                    </span>
-                    <span class="card-text">
-                        No of Vacancies:{{ $job->vacancy }}
-                    </span>
+                    <div>
+                        <h1>{{ $job->title }}</h1>
+
+                        <div class="d-flex justify-content-center">
+                            <span class="card-text" style="margin-right:40px;">
+                                Job Type: {{ $job->type }}
+                            </span>
+                            <span class="card-text">
+                                No of Vacancies:{{ $job->vacancy }}
+                            </span>
+                        </div>
+                        <div>
+                            <div class="d-flex justify-content-center">
+                                <a href="{{ route('applications.create', $job) }}">Apply Now</a>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div class="d-flex justify-content-center">
-                    <a href="{{ route('applications.create',$job) }}">Apply Now</a>
-                </div>
+
             </div>
             <div class="show-job-img">
-                <img src="" alt="">
+                <img src="url(/frontend/images/showjob.png)" alt="">
             </div>
         </div>
     </section>
@@ -64,7 +74,7 @@
                 <div class="col-md-4">
                     <div class="job-details-show-info-right">
                         <div class="d-flex justify-content-center">
-                            <a href="{{ route('applications.create',$job) }}">Apply Now</a>
+                            <a href="{{ route('applications.create', $job) }}">Apply Now</a>
                         </div>
                         <h5>Job Summary</h5>
                         <div class="job-details-show-info-right-body">
